6ea79147dce7f6ad632aa095671badd8a420b19c
JP Dillingham <jpdillingham@gmail.com>
2016-08-13 13:29:24 -0500

    Corrected assembly info

--- RealtimeLogger/Properties/AssemblyInfo.cs
+++ RealtimeLogger/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RealtimeLogger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NLog.RealtimeLogger")]
+[assembly: AssemblyDescription("A C# class that works in conjunction with the NLog 'MethodCall' logging target to expose log messages via an event in real time.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RealtimeLogger")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("NLog.RealtimeLogger")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 JP Dillingham (jp@dillingham.ws)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.4")]
+[assembly: AssemblyFileVersion("0.9.0.4")]
